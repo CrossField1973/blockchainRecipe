3a1edbeca4c20811e404669ee564d837c37b4895
Kevin <kevin.weis@gmx.net>
2021-05-10 17:23:45 +0200

    f

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,11 +1,7 @@
 const Migrations = artifacts.require("Migrations");
-// const DHBWCoin = artifacts.require("DHBWCoin");
-// const DHBWVoting = artifacts.require("DHBWVoting");
 const PadlockToken = artifacts.require("PadlockToken");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
-  // deployer.deploy(DHBWCoin);
-  // deployer.deploy(DHBWVoting);
   deployer.deploy(PadlockToken);
 };
